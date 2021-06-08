eed6eff6cd7d9e90b0f922088064bae272dd5776
Sam Roy-Effer <samroyeffer@gmail.com>
2021-06-08 11:02:22 -0400

    added some text to identify what does what

--- express_server.js
+++ express_server.js
@@ -1,48 +1,54 @@
 const express = require("express");
 const app = express();
-const PORT = 8080; 
+const PORT = 8080;
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  // memory of url codes
+  b2xVn2: "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com",
 };
 
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  res.send("Hello!"); // initial homepage
 });
 app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+  res.json(urlDatabase); // json page of the database
 });
 app.get("/hello", (req, res) => {
+  // practice page
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 app.get("/urls", (req, res) => {
-  const templateVars = {urls: urlDatabase};
-  res.render("url_index", templateVars);
-});
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-});
-app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = {shortURL: req.params.shortURL, longURL: req.params.longURL};
-  res.render("url_show", templateVars);
+  const templateVars = { urls: urlDatabase };
+  res.render("url_index", templateVars); // list of saved urls
 });
 app.post("/urls", (req, res) => {
   console.log(req.body);
-  res.send("Ok");         
+  res.send("Ok");
+});
+app.post("/urls_added", (req, res) => {});
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new"); // adding new urls
+});
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: req.params.longURL,
+  };
+  res.render("url_show", templateVars);
 });
 const generatRandomString = (length) => {
-  const char = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let code = ' ';
+  const char = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let code = " ";
   const charLength = char.length;
   for (let charater of char) {
     code += char.charAt(Math.floor(Math.random() * charLength));
   }
   return code;
-}
+};
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
