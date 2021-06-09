259b295cd99016b9d2bb4c9172bcbea97ab34bec
Sam Roy-Effer <samroyeffer@gmail.com>
2021-06-09 14:29:24 -0400

    Changed the views and header to be more functional, added logout button and working function

--- express_server.js
+++ express_server.js
@@ -1,9 +1,10 @@
+const cookieParser = require("cookie-parser");
 const express = require("express");
 const app = express();
 const PORT = 8080;
-//app.use(express.json({ extented: true }));
 app.use(express.urlencoded({ extented: true }));
 app.set("view engine", "ejs");
+app.use(cookieParser());
 
 const generateRandomString = () => Math.random().toString(36).substr(2, 8);
 const urlDatabase = {
@@ -12,51 +13,92 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com",
 };
 
+//creating a new url
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new"); // adding new urls
-});
-app.get("/urls/:shortURL", (req, res) => {
+  console.log('added a new url')
   const templateVars = {
-    //template
-    shortURL: req.params.shortURL,
-    longURL: urlDatabase[req.params.shortURL],
-  };
-  console.log(urlDatabase);
-  res.render("url_show", templateVars);
-});
-app.get("/", (req, res) => {
-  res.redirect("/urls"); // initial homepage
-});
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase); // json page of the database
+    username: req.cookies["username"],
+  
+  }
+  res.render("urls_new", templateVars); 
 });
 
+// adding a long url to make shor url 
+app.get("/urls/:shortURL", (req, res) => {
+  console.log('')
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"],
+    
+  };
+  console.log(urlDatabase);
+  
+  res.render("url_show", templateVars);
+});
+
+app.get('/logout', function(req, res) {
+  res.clearCookie("username")
+  res.redirect("/")
+})
+
+// redirects to urls page
+app.get("/", (req, res) => {
+  res.redirect("/urls"); 
+});
+
+
+// show list of urls
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, 
+    username: req.cookies["username"] };
   res.render("url_index", templateVars); // list of saved urls
 });
+
+// gets the short urls and send the user to the longurl page
 app.get("/u/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   const fetchedURL = urlDatabase[shortURL];
   console.log(fetchedURL);
   res.redirect(fetchedURL);
 });
+
+//get the login infor and check to see if it exists
+app.get("/login", (req, res) => {
+  let templateVars = {
+    username: req.cookies["username"],
+  };
+  res.redirect("/urls");
+});
+
+// checks the user when logged in send to the home page
+app.post("/login", (req, res) => {
+  let username = req.body.username;
+  res.cookie("username", username);
+  res.redirect("/urls");
+});
+
+// shows the new short url code for the longurl
 app.post("/urls", (req, res) => {
-  console.log(JSON.stringify(req.body));
+  console.log("Showing url page",JSON.stringify(req.body));
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
-  res.redirect(`/urls/${shortURL}`);
+  res.redirect('/urls');
 });
+
+//removes a short and long url
 app.post("/urls/:shortURL/delete", (req, res) => {
   const { shortURL } = req.params;
   console.log(shortURL);
   delete urlDatabase[shortURL];
   res.redirect("/urls");
 });
+
+//edits the long url associated with the short url
 app.post("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   urlDatabase[shortURL] = req.body.longURL;
-  res.redirect(`/urls/${shortURL}`);
+  res.redirect('/urls');
 });
 
 app.listen(PORT, () => {
